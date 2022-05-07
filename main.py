93cce785fc195d2f647db9ec96b96009c357bd65
aravindakumar79 <37686588+aravindakumar79@users.noreply.github.com>
2022-05-07 10:39:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print ("Welcome to Pyhton automation lab")
 print ("Project done by Aravinda Kumar V")
 print ("final project")
-Print ("error message from python")
+print ("error message from python")
